8ea5b6522f6bfde31d1a2663c9f573590839e818
Yuriy <zui0109@mail.ru>
2023-06-24 15:27:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ from colorama import init
 from colorama import Fore, Back, Style
 
 init()
+
 print(Fore.RED)
+os.system(f"mode con:cols=150 lines=30")
 tprint("VXRGXS", "rand", space=1)
 
 ctypes.windll.kernel32.SetConsoleTitleA(b"MMRapp")
@@ -32,7 +34,7 @@ while work:
             sub_text = text[:text.find("\"Log\"")]  # обрезка первой части "Log"
             sub_text1 = sub_text[:sub_text.rfind("]}") + 2]  # удаление "Log" в первой части
             if len(sub_text1) < 2:
-                print("Реплей сломан")
+                print(Fore.RED, "Не удается открыть реплей")
                 continue
             block1 = json.loads(sub_text1)  # 1 блок логов
             players = block1["7"]  # Список игроков в 1 блоке
@@ -50,49 +52,52 @@ while work:
                     print(Fore.BLUE + "\nКоманда #1")
                 if i == 4:
                     print(Fore.BLUE + "\nКоманда #2")
-                player = players[i]  # берем игрока
-                playerOper = player["8"]  # берем доп информацию об игроке
+                player = players[i]
+                playerOper = player["8"]
                 print(
-                    f"""{Fore.RED}{playerOper["1"]}{Fore.MAGENTA}\tID группы: {Fore.RED}{player["1"]}{Fore.MAGENTA}\tID игрока: {Fore.RED}{player["0"]}{Fore.MAGENTA}\tУровень: {Fore.RED}{player["3"]}{Fore.MAGENTA}\tНикнейм: {Fore.RED}{player["2"]}{Fore.MAGENTA}""")
+                    f"""{Fore.MAGENTA}Оперативник: {Fore.RED}{("{0:<10}").format(playerOper["1"])}{Fore.MAGENTA}\tID группы: {Fore.RED}{("{0:<7}").format(player["1"])}{Fore.MAGENTA}\tID игрока: {Fore.RED}{("{0:<7}").format(player["0"])}{Fore.MAGENTA}\tУровень: {Fore.RED}{("{0:<2}").format(player["3"])}{Fore.MAGENTA}\tНикнейм: {Fore.RED}{("{0:<10}").format(player["2"])}{Fore.MAGENTA}""")
 
             # Вывод доп инфы
             while True:
                 word = input(
                     f"""{Fore.BLUE}\ninfo{Fore.GREEN} - Доп информация об игроках\n{Fore.BLUE}rep{Fore.GREEN} - к списку реплеев\n{Fore.BLUE}exit{Fore.GREEN} - выход\n""")
-                os.system('CLS')
                 if word == "info":
                     for i in range(int(pveregime.get(regime))):
                         player = players[i]
                         playerOper = player["8"]
                         print(Fore.BLUE, i, Fore.GREEN, player["2"], Fore.BLUE)
+                        if i == 3:
+                            print("")
                     numberPlayer = input(
                         "\n*********************\nВведите номер игрока, чтобы посмотреть дополнительную информацию:\n")
-                    if numberPlayer.isdigit():
-                        if int(numberPlayer) < int(pveregime.get(regime)):
-                            player = players[int(numberPlayer)]
-                            playerOper = player["8"]
-                            print(
-                                f"""
+                    if numberPlayer.isdigit() and int(numberPlayer) < int(pveregime.get(regime)):
+                        player = players[int(numberPlayer)]
+                        playerOper = player["8"]
+                        print(
+                            f"""
 {Fore.RED}{player["2"]}\n
-{Fore.MAGENTA}Оперативник:            {Fore.RED}{playerOper["18"]}
+{Fore.MAGENTA}Оперативник:            {Fore.RED}{playerOper["1"]}
 {Fore.MAGENTA}Резервы:                {Fore.RED}{playerOper["14"]}
 {Fore.MAGENTA}Установленные навыки:   {Fore.RED}{playerOper["15"]}
 {Fore.MAGENTA}Уровень оперативника:   {Fore.RED}{playerOper["18"]}
 {Fore.MAGENTA}Престижа оперативника:  {Fore.RED}{playerOper["19"]}
 {Fore.MAGENTA}Опыт оперативника:      {Fore.RED}{playerOper["4"]}
 
-{Fore.MAGENTA}Все навыки игрока:
+{Fore.MAGENTA}Навыки аккаунта:
 {Fore.RED}{player["20"]}
 """)
-                        else:
-                            print(Fore.RED, "\n***Введен неверный номер***")
                     else:
                         print(Fore.RED, "\n***Введен неверный номер***")
+                        break
                 elif word == "exit":
                     work = False
                     break
                 elif word == "rep":
+                    os.system('CLS')
                     break
+                else:
+                    print(Fore.RED, "\nОшибка")
+                    continue
         except:
             print(Fore.RED, "\n***Не удается открыть реплей***")
 
